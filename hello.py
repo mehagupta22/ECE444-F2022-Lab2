551c2b578a59b9be7934ebeeda2ebf7f6c731de2
Meha Gupta <mehagupta22@gmail.com>
2022-09-27 19:10:53 -0400

    Activity-3-2

--- hello.py
+++ hello.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return '<h1>Hello World!</h1>'
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return '<h1>Hello, {}!</h1>'.format(name)

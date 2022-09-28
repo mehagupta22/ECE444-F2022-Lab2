9bc1d7b7e9b066b82cfc09fe72826999929829b6
Meha Gupta <mehagupta22@gmail.com>
2022-09-27 20:29:23 -0400

    Completed Activity-4-add-email

--- hello.py
+++ hello.py
@@ -14,6 +14,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -32,8 +33,12 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_name != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email = session.get('email'))
